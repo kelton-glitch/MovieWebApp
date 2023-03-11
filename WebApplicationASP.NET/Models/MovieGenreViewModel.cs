0ab86aff00b8fc0a3c823535102a7b3f8da25e56
Kamgo N Elton <kamgoelton89@gmail.com>
2023-03-11 09:25:15 +0100

    validation

--- WebApplicationASP.NET/Models/MovieGenreViewModel.cs
+++ WebApplicationASP.NET/Models/MovieGenreViewModel.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 
-namespace MvcMovie.Models
+namespace MvcMovie.Models;
+
+public class MovieGenreViewModel
 {
-    public class MovieGenreViewModel
-    {
-        public List<Movie>? Movies { get; set; }
-        public SelectList? Genres { get; set; }
-        public string? MovieGenre { get; set; }
-        public string? SearchString { get; set; }
-    }
+    public List<Movie>? Movies { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
 }
