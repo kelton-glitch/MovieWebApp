24ea523cd106a538201f65140c4c64011b8ed8c6
Kamgo N Elton <kamgoelton89@gmail.com>
2023-03-15 12:15:48 +0100

    added validation

--- WebApplicationASP.NET/Models/Movie.cs
+++ WebApplicationASP.NET/Models/Movie.cs
@@ -9,7 +9,7 @@ public class Movie
 {
     public int Id { get; set; }
     [StringLength(60, MinimumLength = 3)]
-    [Required]
+    //[Required]
     public string? Title { get; set; }
 
     [Display(Name = "Release Date")]
@@ -17,7 +17,7 @@ public class Movie
     public DateTime ReleaseDate { get; set; }
 
     [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
-    [Required]
+    //[Required]
     [StringLength(30)]
     public string? Genre { get; set; }
 
@@ -28,6 +28,6 @@ public class Movie
 
     [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
     [StringLength(5)]
-    [Required]
+    //[Required]
     public string? Rating { get; set; }
 }
